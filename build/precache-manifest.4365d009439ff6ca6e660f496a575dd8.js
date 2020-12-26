6e69ed436c476bc65f86e361ed9197a9b48a9e9d
announaymen <a.announ@esi-sba.dz>
2020-12-26 14:47:38 +0000

    commit

---
+++ build/precache-manifest.4365d009439ff6ca6e660f496a575dd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e1a037c510c2498a9ab4bf3e7e49ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f13a46adaf603fbcfe8",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "ba899f7be8264c301615",
+    "url": "/static/js/2.5b726268.chunk.js"
+  },
+  {
+    "revision": "0f13a46adaf603fbcfe8",
+    "url": "/static/js/main.48771d08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>TP INFOVIZ</h1>
+      <h1>TP InfoViS </h1>
       <D3BarGraph />
     </div>
   );
