aedb24fcbf862f00ff10633b4a7f4870d0c7368a
announaymen <a.announ@esi-sba.dz>
2020-12-26 15:31:08 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>TP InfoViS </h1>
+      <h2>
+        pour voir le code source cliquer{" "}
+        <a href="https://github.com/announaymen/DataVizWithD3">ici</a>{" "}
+      </h2>
       <D3BarGraph />
     </div>
   );
