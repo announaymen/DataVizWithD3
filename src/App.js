d29fbd0b28c95e5fc81ea2cf9eaf0f70533d1826
announaymen <a.announ@esi-sba.dz>
2020-12-26 14:43:08 +0000

    2nd commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>TP INFOVIZ</h1>
       <D3BarGraph />
     </div>
   );
