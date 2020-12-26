57c59d48449e0b1a37a151cb5191306febc0ce5b
announaymen <a.announ@esi-sba.dz>
2020-12-26 15:42:33 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>TP InfoViS </h1>
-      <h2>
-        pour voir le code source cliquer{" "}
-        <a href="https://github.com/announaymen/DataVizWithD3">ici</a>{" "}
-      </h2>
+      <h3>
+        Cette visualisation a été créée avec la bibliothèque D3 et de React et a
+        été déployée sur Heroku <br />
+        pour voir le code source cliquer &nbsp;
+        <a href="https://github.com/announaymen/DataVizWithD3" target="_blank">
+          ici
+        </a>
+      </h3>
+      <p>
+        {" "}
+        - ANNOUN Aymen <br /> - BENDAHO Riyadh{" "}
+      </p>
       <D3BarGraph />
     </div>
   );
